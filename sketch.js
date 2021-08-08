ce8a64510448e8b82908ced177e465283a44ace6
Saman <87480225+SamanAsim@users.noreply.github.com>
2021-08-08 21:43:51 +1000

    New Files added
    New code files are added to the repository

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var seapng= createSprite (200,100,30,30);
+seapng.setAnimation("seaImg");
+seapng.scale= 0.5;
+
+
+
+function preload(){
+
+}
+
+seaImg= loadImage("sea.png");
+shipImg1= loadAnimation("ship-1,ship-2,ship-3,ship-4")
+
+spriteName.addImage(sea.Img);
+function setup(){
+  createCanvas(400,400);
+  
+}
+
+function draw() {
+  background("blue");
+}
